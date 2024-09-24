0de430f5b685a85cb232c3842cf9b8fc513f368d
Chuey Okoye <chuey9218@gmail.com>
2024-09-24 15:48:28 +0100

    Add new and create actions and form for creating tasks

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: :task
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
   # Defines the root path route ("/")
   # root "posts#index"
 end

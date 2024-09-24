5724a48c6da74ec1d112c61066ef727e057e0b3d
Chuey Okoye <chuey9218@gmail.com>
2024-09-24 16:14:54 +0100

    Add destroy action and delete links with confirmation for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
   get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show", as: :task
   post "tasks", to: "tasks#create"
+
+  delete "tasks/:id", to: "tasks#destroy", as: :delete_task
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
